744d564523a176e2a3381b662abe1e2f99b928b4
Giuseppe Battistella <bartgb00@gmail.com>
2019-06-12 19:00:59 -0300

    Added public folder

--- server/src/controllers/finance.spec.js
+++ server/src/controllers/finance.spec.js
@@ -5,71 +5,38 @@ const db = require(`../models`)
 
 describe(`getVariation`, () => {
   describe(`calculates variation correctly`, () => {
-    test(`10 -> 110 = 900%`, async () => {
-      // Arrange
-      db.finance.selectOne.mockImplementation(async ({ orderAsc }) => {
-        if (orderAsc)
+    ;[
+      [25, 20, -20],
+      [100, 1, -99],
+      [123, 123, 0],
+      [400, 408, 2],
+      [25, 30, 20],
+      [10, 11, 10],
+      [10, 100, 900],
+      [1, 30, 2900],
+    ].forEach(([from, to, variation]) => {
+      test(`${from} -> ${to} = ${variation}%`, async () => {
+        // Arrange
+        db.finance.selectOne.mockImplementation(async ({ orderAsc }) => {
+          if (orderAsc)
+            return {
+              rows: [{ bid: from }],
+            }
+
           return {
-            rows: [{ bid: 10 }],
+            rows: [{ bid: to }],
           }
+        })
 
-        return {
-          rows: [{ bid: 110 }],
-        }
+        // Act
+        const result = financeController.getVariation({
+          code: `USD`,
+          startDate: new Date(),
+        })
+
+        // Assert
+        await expect(result).resolves.toEqual(variation)
       })
-
-      // Act
-      const result = financeController.getVariation({
-        code: `USD`,
-        startDate: new Date(),
-      })
-
-      // Assert
-      await expect(result).resolves.toEqual(1000)
-    })
-    test(`10 -> 11 = 10%`, async () => {
-      // Arrange
-      db.finance.selectOne.mockImplementation(async ({ orderAsc }) => {
-        if (orderAsc)
-          return {
-            rows: [{ bid: 10 }],
-          }
-
-        return {
-          rows: [{ bid: 11 }],
-        }
-      })
-
-      // Act
-      const result = financeController.getVariation({
-        code: `USD`,
-        startDate: new Date(),
-      })
-
-      // Assert
-      await expect(result).resolves.toEqual(10)
-    })
-    test(`25 -> 30 = 20%`, async () => {
-      // Arrange
-      db.finance.selectOne.mockImplementation(async ({ orderAsc }) => {
-        if (orderAsc)
-          return {
-            rows: [{ bid: 25 }],
-          }
-
-        return {
-          rows: [{ bid: 30 }],
-        }
-      })
-
-      // Act
-      const result = financeController.getVariation({
-        code: `USD`,
-        startDate: new Date(),
-      })
-
-      // Assert
-      await expect(result).resolves.toEqual(20)
     })
   })
 })
